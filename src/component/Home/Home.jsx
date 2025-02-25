5280aa97c902daea21e921250fe590dff072d06c
Gharsa Amin <ghars0718@gmail.com>
2025-02-24 19:02:42 -0500

    created a header section for the home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -5,6 +5,12 @@ import { Link } from "react-router-dom";
 export default function Home() {
 	return (
 		<>
+			<p>Quantum is for Everyone</p>
+			<p>Blockchain is for Everyone</p>
+			<p>
+				Let's make quantum decentralized and let's blockchain secure, beyond
+				just the 0s, and 1s.
+			</p>
 			<img src="./Hero-Quantum-Blocks.png" />
 			<Link to="/QuantumBooks">
 				<h2>Quantum Books</h2>
